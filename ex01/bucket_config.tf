d168b95e5c34f2476d81c5e41b6e00f229674b62
ayaarab <ayoubaarab571@gmail.com>
2025-12-09 19:10:15 +0100

    skill issues :/

--- ex01/bucket_config.tf
+++ ex01/bucket_config.tf
@@ -1,12 +1,28 @@
-provider "aws" {
-  region = "us-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 
+provider "aws" {
   access_key = "minioadmin"
   secret_key = "minioadmin"
-  endpoint = "http://localhost:9000"
-  s3_force_path_style = true # Required for MinIO
+  region     = "us-east-1"
+
+  # Make the AWS provider talk to a local MinIO server (S3-compatible)
+  s3_force_path_style      = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+
+  endpoints {
+    s3 = "http://localhost:9000"
+  }
 }
 
 resource "aws_s3_bucket" "raw-data-bucket" {
-	bucker = "raw-data"
+  bucket = "raw-data"
+  acl    = "private"
 }
